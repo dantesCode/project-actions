e7815872e000b5c69b564d65277213bf830ef65b
dantesCode <juanjo.xboxlive@gmail.com>
2026-04-20 17:39:19 +0200

    fix: makefile detector reports actual filename as source

--- src/commands/placementCommands.ts
+++ src/commands/placementCommands.ts
@@ -2,17 +2,21 @@ import * as vscode from "vscode";
 
 export interface PlacementManagers {
   statusBar: { openPicker(): void };
+  editorTitle: { openPicker(): void };
 }
 
 export function registerPlacementCommands(
   context: vscode.ExtensionContext,
   managers: PlacementManagers,
 ): vscode.Disposable[] {
-  const { statusBar } = managers;
+  const { statusBar, editorTitle } = managers;
 
   return [
     vscode.commands.registerCommand("projectActions.openStatusBarPicker", () => {
       statusBar.openPicker();
+    }),
+    vscode.commands.registerCommand("projectActions.openEditorTitlePicker", () => {
+      editorTitle.openPicker();
     }),
   ];
 }
